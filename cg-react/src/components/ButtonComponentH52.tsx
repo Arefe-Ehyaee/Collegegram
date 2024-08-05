990631185c8da970ca79afc08df035f15890ad75
Ali Ansari <alee.ansari@gmail.com>
2024-08-05 07:06:10 +0000

    Resolve "button component"

---
+++ cg-react/src/components/ButtonComponentH52.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface ButtonProps {
+  text: string;
+  iconsrc?: string;
+  styling?: string;
+  handleOnClick?:React.FC
+
+}
+const CustomButtonH52: React.FC<ButtonProps> = ({ text, iconsrc, styling,handleOnClick }) => {
+  return (
+    <button onClick={handleOnClick}
+      className={`flex h-52 items-center gap-2 rounded-3xl px-12 py-4 text-sm text-white font-isf subpixel-antialiased ${styling}`}
+      dir="rtl"
+    >
+      {iconsrc && <img src={iconsrc} alt="logo" className="h-3.5 w-3.5" />}
+      <span>{text}</span>
+    </button>
+  );
+};
+
+export default CustomButtonH52;
