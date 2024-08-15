29b4b16445eb934d3f79a47f6591c1d462ee6333
Ali Ansari <alee.ansari@gmail.com>
2024-08-15 13:23:41 +0000

    Resolve "Progress stepper Component"

--- cg-react/src/components/upload-edit-posts/UploadPostsModal.tsx
+++ cg-react/src/components/upload-edit-posts/UploadPostsModal.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import stepper1 from '../../assets/icons/stepper1.svg'
+
 
 const UploadPostsModal = () => {
   return (
-    <div>UploadPostsModal</div>
+    <div><img src={stepper1} alt="" /></div>
   )
 }
 
