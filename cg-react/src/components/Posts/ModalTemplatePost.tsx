7c31a533273025ebb16fe98b2eb63fd508479986
arefe.ehyaee <arefe.ehyaee@gmail.com>
2024-08-17 06:26:15 +0000

    Resolve "Improvement responsive design"

---
+++ cg-react/src/components/Posts/ModalTemplatePost.tsx
@@ -0,0 +1,34 @@
+
+import { Children } from "react";
+
+interface ModalPostProps {
+  showModal: boolean;
+  onClose?: () => void;
+  styling?: string;
+  mainComponent?: JSX.Element;
+  children?:React.ReactNode
+}
+
+const ModalTemplatePost = ({
+  showModal,
+  onClose,
+  styling,
+  mainComponent,
+  children
+}: ModalPostProps) => {
+  return (
+    <div
+      className={`fixed inset-0 flex flex-col items-center justify-center bg-siah/40 ${showModal ? "visible bg-siah/40" : "invisible"}`}
+      onClick={onClose}
+    >
+      <div
+        className="flex flex-col items-center justify-center rounded-3xl border-khakeshtari-400 bg-khakeshtari-100 px-[35px] py-16"
+        onClick={(e) => e.stopPropagation()}
+      >
+        {mainComponent}{children}
+      </div>
+    </div>
+  );
+};
+
+export default ModalTemplatePost;
